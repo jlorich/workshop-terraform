2fb76f433a161ecb9acb93f50fb6eb748898c927
Joey Lorich <jolorich@microsoft.com>
2019-05-10 09:34:46 -0600

    update default container

--- sample/main.tf
+++ sample/main.tf
@@ -1,11 +1,11 @@
 variable "environment" {
     type = "string"
-    description = "The desired deployment enviornment name"
+    description = "The desired deployment environment name"
     default = "dev"
 }
 
 resource "azurerm_resource_group" "default" {
-  name     = "terraform-pipelines-workshop-${var.enviornment}-rg"
+  name     = "terraform-pipelines-workshop-${var.environment}-rg"
   location = "West US"
 }
 resource "azurerm_app_service_plan" "default" {
@@ -22,13 +22,13 @@ resource "azurerm_app_service_plan" "default" {
 }
 
 resource "azurerm_app_service" "default" {
-  name                = "terraform-pipelines-workshop-myname-${var.enviornment}-app"
+  name                = "terraform-pipelines-workshop-myname-${var.environment}-app"
   location            = "${azurerm_resource_group.default.location}"
   resource_group_name = "${azurerm_resource_group.default.name}"
   app_service_plan_id = "${azurerm_app_service_plan.default.id}"
 
   site_config = {
     always_on        = true
-    linux_fx_version = "DOCKER|hello-world"
+    linux_fx_version = "DOCKER|nginxdemos/hello"
   }
 }
